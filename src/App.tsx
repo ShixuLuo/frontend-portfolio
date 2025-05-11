f3c2f0fed5253fce94589e0b49a90a56acdb04e4
Shixu Luo <git@luo.sx>
2025-05-11 13:38:51 -0400

    fix: update react router basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import MainPage from './pages/MainPage';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import PasswordGenerator from './standalones/password-generator-app/App';
 
+const basename = import.meta.env.BASE_URL;
+
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="components/password-generator-app" element={<PasswordGenerator />} />
